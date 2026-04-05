49a1f6d105a75fa7283faeef4e2d0c2b6db0ef3f
sultan.dev <sultanaltynbek17@gmail.com>
2026-04-06 01:23:27 +0500

    added app

--- core_app/serializers.py
+++ core_app/serializers.py
@@ -1,11 +1,32 @@
 from rest_framework import serializers
-from .models import Ad
+from .models import Ad, Review, Category, City
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Review
+        fields = ['id', 'author_username', 'text', 'rating', 'created_at']
 
 class AdSerializer(serializers.ModelSerializer):
     author_username = serializers.ReadOnlyField(source='author.username')
     category_name = serializers.ReadOnlyField(source='category.name')
     city_name = serializers.ReadOnlyField(source='city.name')
+    
+    # Достаем все отзывы, связанные с этим объявлением (related_name='reviews' в модели)
+    reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
         model = Ad
-        fields = ['uuid', 'title', 'description', 'price', 'image', 'author_username', 'category_name', 'city_name', 'created_at']
+        fields = [
+            'uuid', 
+            'title', 
+            'description', 
+            'price', 
+            'image', 
+            'author_username', 
+            'category_name', 
+            'city_name', 
+            'created_at',
+            'reviews'  # Теперь отзывы полетят во Флаттер вместе с объявой
+        ]
